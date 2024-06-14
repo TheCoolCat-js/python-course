89967d43d00575aa84e3bc30bc64f0619d1bfe32
Luca2k <luca2010meze@gmail.com>
2024-06-14 22:43:32 +0300

    update name.py, put an login

--- name.py
+++ name.py
@@ -1,2 +1,24 @@
+# name = input("Name: ")
+# print(f"Hello, {name}")
+
+# Login
+
+# name = input("Name: ")
+# password = input("Password: ")
+
+# if name == "admin" and password == "password":
+#     print("Login successful")
+# else:
+#     print("Invalid credentials")
+
+# Admin login
+
 name = input("Name: ")
-print(f"Hello, {name}")
+repeatName = input("Repeat name: ")
+password = input("Password: ")
+repeatPassword = input("Repeat Password: ")
+
+if name == name and password == repeatPassword:
+    print("Login successful")
+else:
+    print("Invalid credentials")
